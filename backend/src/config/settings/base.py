4cc24f7b0d4ba306e0f2bfc6390e1ffa892e7c5e
Pierre Claver Koko Banywerha <pierreclaverkoko@gmail.com>
2026-07-25 11:25:04 +0100

    CHanges for all

--- backend/src/config/settings/base.py
+++ backend/src/config/settings/base.py
@@ -145,8 +145,18 @@ STATICFILES_DIRS = [BASE_DIR / 'static']
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
 
-DEFAULT_FROM_EMAIL = config('DEFAULT_FROM_EMAIL', default='noreply@vaxiil.local')
-SITE_URL = config('SITE_URL', default='http://localhost:8000')
+SITE_URL = config('SITE_URL', default='https://vaxiiltropbien.com')
+
+# settings.py using Resend SMTP
+
+EMAIL_BACKEND = config('EMAIL_BACKEND', default='django.core.mail.backends.smtp.EmailBackend')
+EMAIL_HOST = config('EMAIL_HOST', default='smtp.resend.com')
+EMAIL_PORT = config('EMAIL_PORT', default=587, cast=int)
+EMAIL_USE_TLS = config('EMAIL_USE_TLS', default=True, cast=bool)
+EMAIL_HOST_USER = config('EMAIL_HOST_USER', default='')
+EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD', default='')
+
+DEFAULT_FROM_EMAIL = config('DEFAULT_FROM_EMAIL', default='Vaxiil <send@vaxiiltropbien.com>')
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
